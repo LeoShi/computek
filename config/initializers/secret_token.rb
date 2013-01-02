96bddffbee2624e8d226364d844227d48d168990
Shi Lei <mleoshi@gmail.com>
2013-01-02 23:05:19 +0200

    SL: init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Computek::Application.config.secret_token = '66636fae1af16e504ef184ef8f3dd9147b64e4c4174415e960a6fa9ebfc598b9b318ca86c4baf5caac82b290f13a9d6a940c3c9d70ca0057830cecd5436c64ae'
